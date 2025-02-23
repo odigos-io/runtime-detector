1eb93dfb08c2a04a3eab48737c020e7248fe1fcc
Ron Federman <ron@keyval.dev>
2025-02-23 10:47:37 +0200

    make generate and add tests

--- internal/probe/probe_test.go
+++ internal/probe/probe_test.go
@@ -34,6 +34,18 @@ func TestLoad(t *testing.T) {
 		err := p.load(uint32(4026532561))
 		defer p.Close()
 		assert.NoError(t, err)
+
+		m := p.c.Maps[envPrefixMapName]
+		assert.NotNil(t, m)
+
+		value := bpfEnvPrefixT{}
+		err = m.Lookup(uint32(0), &value)
+		assert.NoError(t, err)
+		assert.Equal(t, uint64(len("TEST")), value.Len)
+
+		prefixStr := make([]byte, len("TEST"))
+		copy(prefixStr, value.Prefix[:])
+		assert.Equal(t, []byte("TEST"), prefixStr)
 	})
 
 	t.Run("load with too long env prefix", func(t *testing.T) {
@@ -62,6 +74,50 @@ func TestLoad(t *testing.T) {
 			containerPID, err := p.GetContainerPID(pid)
 			assert.NoError(t, err)
 			assert.Equal(t, 0, containerPID)
+		}
+	})
+
+	t.Run("load with too long file name", func(t *testing.T) {
+		p := &Probe{
+			logger: slog.Default(),
+			openFilesToTrack: []string{repeatedString(129, "a")},
+		}
+		err := p.load(uint32(4026532561))
+		defer p.Close()
+		assert.ErrorContains(t, err, "filename is too long")
+	})
+
+	t.Run("load with too many file names", func(t *testing.T) {
+		p := &Probe{
+			logger: slog.Default(),
+			openFilesToTrack: make([]string, 9),
+		}
+		err := p.load(uint32(4026532561))
+		defer p.Close()
+		assert.ErrorContains(t, err, "too many files to track")
+	})
+
+	t.Run("load with multiple file names", func(t *testing.T) {
+		p := &Probe{
+			logger: slog.Default(),
+			openFilesToTrack: []string{"/var/file1.so", "/var/file2.so"},
+		}
+		err := p.load(uint32(4026532561))
+		defer p.Close()
+		assert.NoError(t, err)
+
+		m := p.c.Maps[filenameMapName]
+		assert.NotNil(t, m)
+
+		for i, file := range p.openFilesToTrack {
+			value := bpfFilenameT{}
+			err = m.Lookup(uint32(i), &value)
+			assert.NoError(t, err)
+			assert.Equal(t, uint64(len(file)), value.Len)
+
+			filename := make([]byte, len(file))
+			copy(filename, value.Buf[:])
+			assert.Equal(t, []byte(file), filename)
 		}
 	})
 }
